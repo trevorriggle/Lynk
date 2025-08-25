7271d33187dd99529a4012fd793fe624eb00c5c4
trevorriggle <155181481+trevorriggle@users.noreply.github.com>
2025-08-25 11:49:26 -0400

    x

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Header from "./components/Header";
+import Header from "../components/Header";
 import LeftStack from "./components/LeftStack";
 import RightPanel from "./components/RightPanel";
 import Chat from "./components/Chat";
