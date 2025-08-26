d665cd0343d819a3f5266df1f2272c72f402c3d1
trevorriggle <155181481+trevorriggle@users.noreply.github.com>
2025-08-26 11:43:51 -0400

    z

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ import TopBar from "./components/TopBar";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "900"],
+  weight: ["400", "900"], // 400 Regular, 900 Black
   variable: "--font-poppins",
 });
 
@@ -19,35 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={poppins.variable}>
       <body className="min-h-screen">
         <TopBar />
-        {/* Push content below the fixed bar */}
         <div className="pt-12">{children}</div>
-      </body>
-    </html>
-  );
-}
-
-// If you created the top bar earlier, keep this import. If not, delete the line.
-// import TopBar from "./components/TopBar";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "900"], // 400 = Regular, 900 = Black
-  variable: "--font-poppins",
-});
-
-export const metadata = {
-  title: "HubAI",
-  description: "Conversational workspace",
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className={poppins.variable}>
-      <body>
-        {/* Uncomment if you have TopBar.jsx */}
-        {/* <TopBar /> */}
-        {/* If you use TopBar, add pt-12 here: <div className="pt-12">{children}</div> */}
-        {children}
       </body>
     </html>
   );
